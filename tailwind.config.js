07cf861f777cb62380534519a4448b1d35237903
Delgorithm <delgorithm@gmail.com>
2025-01-16 21:38:47 +0100

    package

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,16 @@
 module.exports = {
   mode: 'jit',
-  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'], // remove unused styles in production
-  darkMode: 'media', // or 'media' or 'class'
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media',
   theme: {
     extend: {
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
-        baskervville: ['Baskervville', 'sans-serif'],
+        baskervville: ['Baskervville', 'serif'],
       },
       backgroundImage: {
         'hero-pattern': "url('/img/bg.webp')",
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
 }
